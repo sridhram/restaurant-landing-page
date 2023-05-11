722cf8dddf4225a100d98c6fc52aea9638f5df1e
Ramkumar S <51109498+sridhram@users.noreply.github.com>
2023-05-11 02:36:47 +0000

    navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,48 +1,61 @@
-import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 import "./Navbar.css";
 import { images } from "../../constants/index";
+import { react, useState } from "react";
 
-const Navbar = () => (
-  <header className="app__header">
-    <a href=".">
-      <img src={images.gericht} alt="logo" />
-    </a>
-    <nav className="app__header--nav">
-      <ul className="app__header--menu">
-        <li>
-          <a className="app__header--under-line" href="#home">
-            Home
-          </a>
-        </li>
-        <li>
-          <a className="app__header--under-line" href="#about">
-            About
-          </a>
-        </li>
-        <li>
-          <a className="app__header--under-line" href="#menu">
-            Menu
-          </a>
-        </li>
-        <li>
-          <a className="app__header--under-line" href="#awards">
-            Awards
-          </a>
-        </li>
-        <li>
-          <a className="app__header--under-line" href="#contact">
-            Contact
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <button className="app__header--book-table-btn">
-      <a className="app__header--under-line" href="#book-table">
-        Book a table
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [showCloseIcon, toggleShowCloseIcon] = useState(false);
+  return (
+    <header className={`app__header ${toggle && "mobile-only slide-bottom"}`}>
+      <a
+        className={
+          toggle ? "app__header--logo mobile-only" : "app__header--logo"
+        }
+        href="."
+      >
+        <img src={images.gericht} alt="logo" />
       </a>
-    </button>
-  </header>
-);
+      <nav className={`app__header--nav ${toggle && "mobile-only"}`}>
+        <ul className={`app__header--menu ${toggle && "mobile-only"}`}>
+          <li className="app__header--under-line">
+            <a href="#home">Home</a>
+          </li>
+          <li className="app__header--under-line">
+            <a href="#about">About</a>
+          </li>
+          <li className="app__header--under-line">
+            <a href="#menu">Menu</a>
+          </li>
+          <li className="app__header--under-line">
+            <a href="#awards">Awards</a>
+          </li>
+          <li className="app__header--under-line">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <button
+        className={`app__header--book-table-btn ${toggle && "mobile-only"}`}
+      >
+        <a href="#book-table">Book a table</a>
+      </button>
+      <button
+        className="mobile-only-menu"
+        onClick={() => {
+          setToggle(!toggle);
+          toggleShowCloseIcon(!showCloseIcon);
+        }}
+      >
+        {!toggle && !showCloseIcon ? (
+          <GiHamburgerMenu fontSize={27} color="#fff" />
+        ) : (
+          <img src={images.logo} />
+        )}
+      </button>
+    </header>
+  );
+};
 
 export default Navbar;
