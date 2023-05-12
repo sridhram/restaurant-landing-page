f87108a01cc5348b158245717752905f28198cb4
Ramkumar S <51109498+sridhram@users.noreply.github.com>
2023-05-12 02:19:07 +0000

    menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './MenuItem.css';
+import "./MenuItem.css";
 
-const MenuItem = () => (
-  <div>
-    MenuItem
-  </div>
+const MenuItem = ({ title, price, tags }) => (
+  <li className="app__menu-item--container">
+    <div className="app__menu-item--price-container">
+      <h3>{title}</h3>
+      <hr />
+      <p className="app__menu-item--price">{price}</p>
+    </div>
+    <p className="app__menu-item--tag">{tags}</p>
+  </li>
 );
 
 export default MenuItem;
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
 
-import './SpecialMenu.css';
+import "./SpecialMenu.css";
+import SubHeading from "../../components/SubHeading/SubHeading";
+import { images, data } from "../../constants/index";
+import MenuItem from "../../components/Menuitem/MenuItem";
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
-  </div>
+  <article className="app__menu--container" id="menu">
+    <SubHeading text="Menu That Fits Your Palatte" />
+    <h1 className="app__menu--heading">Today's special</h1>
+    <section className="app__menu--menu-section">
+      <div>
+        <h2>Wine & Beer</h2>
+        <ul>
+          {data.wines.map((wineData) => {
+            return (
+              <MenuItem
+                title={wineData.title}
+                price={wineData.price}
+                tags={wineData.tags}
+              />
+            );
+          })}
+        </ul>
+      </div>
+      <figure>
+        <img src={images.menu} alt="menu" />
+      </figure>
+      <div>
+        <h2>Cocktails</h2>
+        <ul>
+          {data.cocktails.map((wineData) => {
+            return (
+              <MenuItem
+                title={wineData.title}
+                price={wineData.price}
+                tags={wineData.tags}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+    <button className="app__menu--btn">Know More</button>
+  </article>
 );
 
 export default SpecialMenu;
